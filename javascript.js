29517c2aa2b41591b4f3d0a2a6f2b83b26f5fa39
Scott Chau <scottshing@yahoo.com.hk>
2022-07-24 13:34:35 -0400

    Add color transition  feature

--- javascript.js
+++ javascript.js
@@ -14,26 +14,50 @@ function makeGrid(col, row) {
   mouseOver();
 }
 
+makeGrid(32, 32);
+
 // Highlighted Grid
 function mouseOver() {
   let items = document.querySelectorAll(".item");
   items.forEach((item) => {
     item.addEventListener("mouseover", () => {
-      item.style.backgroundColor = `${randomColor()}`;
+      item.style.setProperty("--red", red());
+      item.style.setProperty("--green", green());
+      item.style.setProperty("--blue", blue());
     }); // adding eventlistener to each div "item"
   });
 }
+let r = 180;
+let g = 180;
+let b = 180;
 
-// Make random color
-function randomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * letters.length)];
+function red() {
+  if (r > 0) {
+    r -= 18;
+    console.log(r);
+  } else if (r <= 0) {
+    r += 180;
   }
-  return color;
+  return r;
 }
-
+function green() {
+  if (g > 0) {
+    g -= 18;
+    console.log(r);
+  } else if (g <= 0) {
+    g += 180;
+  }
+  return g;
+}
+function blue() {
+  if (b > 0) {
+    b -= 18;
+    console.log(b);
+  } else if (b <= 0) {
+    b += 180;
+  }
+  return b;
+}
 function reset() {
   let grid = parseInt(
     prompt("How many grid do you want for your sketch? eg. 16 32 48 64")
@@ -51,5 +75,16 @@ function reset() {
 }
 
 button.addEventListener("click", reset);
+// Make random color
 
-makeGrid(32, 32);
+// function randomColor() {
+
+//   let letters = "0123456789ABCDEF";
+//   let color = "#";
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * letters.length)];
+//   }
+//   return color;
+// }
+
+// reset button
